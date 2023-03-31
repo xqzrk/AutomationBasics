570ca52cf23b7a16b8ad2ff296d45b66097776c8
MSevryukov <smlog@yandex.ru>
2023-03-31 19:53:04 +0300

    TestNG Commit

--- src/test/java/ru/netology/service/CashBackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackServiceTest.java
@@ -10,17 +10,14 @@ public class CashBackServiceTest {
     @Test
     public void shouldFindRemain() {
         int amount = 900;
-
         int expected = 100;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
-
     }
 
     @Test
     public void shouldFindRemainIfAmountEqualsBoundary() {
         int amount = 1000;
-
         int expected = 0;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
@@ -30,7 +27,6 @@ public class CashBackServiceTest {
     @Test
     public void shouldFindRemainIfAmountAboveBoundary() {
         int amount = 1100;
-
         int expected = 900;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
